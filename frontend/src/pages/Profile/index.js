f508fc87e23a8499103a2638a3550731fde17b14
Pedro Carrera <procarrera@gmail.com>
2020-03-27 01:22:30 -0300

    Backend & Frontend

---
+++ frontend/src/pages/Profile/index.js
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from "react";
+import "./styles.css";
+import { Link, useHistory } from "react-router-dom";
+import { FiPower, FiTrash2 } from "react-icons/fi";
+import logoImg from "../../assets/logo.svg";
+import api from "../../services/api";
+
+// COMPONENT STARTS HERE //
+export default function Profile() {
+  const history = useHistory();
+
+  const ongName = localStorage.getItem("ongName");
+  const ongId = localStorage.getItem("ongId");
+  const [incidents, setIncidents] = useState([]);
+
+  useEffect(() => {
+    api
+      .get("profile", {
+        headers: {
+          Authorization: ongId
+        }
+      })
+      .then(response => {
+        setIncidents(response.data);
+      });
+  }, [ongId]);
+
+  async function handleDeleteIncident(id) {
+    try {
+      await api.delete(`incidents/${id}`, {
+        headers: {
+          Authorization: ongId
+        }
+      });
+      setIncidents(incidents.filter(incident => incident.id !== id));
+    } catch (err) {
+      alert("Erro ao deletar caso, tente novamente.");
+    }
+  }
+
+  function handleLogout() {
+    localStorage.clear();
+    history.push("/");
+  }
+
+  return (
+    <div className="profile-container">
+      <header>
+        <img src={logoImg} alt="Be The Hero" />
+        <span className="tooltip">
+          Bem vinda, {ongName} !
+          <span className="tooltiptext">seu ID: {ongId}</span>
+        </span>
+        <Link className="button" to="/incidents/new">
+          Cadastrar novo caso
+        </Link>
+        <button type="button" onClick={handleLogout}>
+          <FiPower size={18} color="#E02041" />
+        </button>
+      </header>
+
+      <h1>Casos cadastrados</h1>
+      <ul>
+        {incidents.map(incident => (
+          <li key={incident.id}>
+            <strong>CASO:</strong>
+            <p>{incident.title}</p>
+            <strong>DESCRIÇÃO:</strong>
+            <p>{incident.description}</p>
+            <strong>VALOR:</strong>
+            <p>
+              {Intl.NumberFormat("pt-BR", {
+                style: "currency",
+                currency: "BRL"
+              }).format(incident.value)}
+            </p>
+            <button
+              onClick={() => handleDeleteIncident(incident.id)}
+              type="button"
+            >
+              <FiTrash2 size={20} color="#a8a8b3" />
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
